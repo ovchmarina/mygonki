6699f9150aef0546b70c5619c67467d6c6b64460
marinaovch <marina-kabakova@yandex.ru>
2020-11-19 15:21:46 +0300

    My three commit

---
+++ MyButton.swift
@@ -0,0 +1,26 @@
+//
+//  MyButton.swift
+//  gonki
+//
+//  Created by user on 11/11/2020.
+//
+
+import UIKit
+
+class MyButton: UIButton {
+    convenience init(type: UIButton.ButtonType) {
+        self.init(type: type)
+    clipsToBounds = true
+
+}
+required init?(coder aDecoder: NSCoder) { super.init(coder: aDecoder) clipsToBounds = true
+}
+override init(frame: CGRect) { super.init(frame: frame) clipsToBounds = true
+    
+}
+override var clipsToBounds: Bool { didSet { layer.cornerRadius = 10
+    
+}
+    
+}
+}
